2862745508ca97a34d4e4c42b5bfbc641135daaf
Anna Bilous <b.anna.v99@gmail.com>
2022-09-15 09:37:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function shaxmat() {
+  let chessBoard = [];
+  let chessRow = [];
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      if ((x + y + 2) % 2 == 0) {
+        chessRow.push("black");
+      } else {
+        chessRow.push("white");
+      }
+    }
+    chessBoard.push(chessRow);
+    chessRow = [];
+  }
+  console.table(chessBoard);
+}
